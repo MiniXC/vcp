0d1b2c558047d1ddf2b17d0585847d1523f90d2f
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2024-06-30 22:06:31 +0200

    remove chess

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,3 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const ssr = false;
 import '$lib/rpgui/rpgui.min.css';
-import '$lib/chessboardjs/css/chessboard-1.0.0.min.css';
